f7d8b8e5391c04e205bfe46416a8528f21ad098e
pradeep.pp <pradeep.pp@gmail.com>
2020-09-27 10:23:52 -0400

    Implemented data driven tests via @DataProvider. Closes #10

---
+++ src/test/java/com/teksenz/demo/TestData.java
@@ -0,0 +1,16 @@
+package com.teksenz.demo;
+
+import org.testng.annotations.DataProvider;
+
+public class TestData {
+
+    @DataProvider(name = "userInfo")
+    public Object[][] loginInfo(){
+        return new Object[][]{
+                {"john", "john123"},
+                {"mary", "mary123"},
+                {"david", "david123"}
+        };
+    }
+
+}
